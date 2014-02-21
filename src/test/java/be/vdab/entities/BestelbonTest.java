2c17970c0d39c521a695632ee9d7ed530d47c1a4
Andy Debaere <andydebaerea@gmail.com>
2014-02-21 11:14:07 +0100

    Test aangepast

--- src/test/java/be/vdab/entities/BestelbonTest.java
+++ src/test/java/be/vdab/entities/BestelbonTest.java
@@ -11,7 +11,7 @@ import static org.junit.Assert.*;
 import be.vdab.valueobjects.BestelbonLijn;
 
 public class BestelbonTest {
-	private Bier bier1, bier2;
+	private Bier bier1;
 	private Set<BestelbonLijn> bestelbonLijnen;
 	private Bestelbon bestelbon;
 
@@ -19,17 +19,14 @@ public class BestelbonTest {
 	public void before() {
 		bier1 = new Bier();
 		bier1.setPrijs(new BigDecimal(5));
-		bier2 = new Bier();
-		bier2.setPrijs(new BigDecimal(10));
 		bestelbonLijnen = new HashSet<>();
 		bestelbonLijnen.add(new BestelbonLijn(2, bier1));
-		bestelbonLijnen.add(new BestelbonLijn(1, bier2));
 		bestelbon = new Bestelbon();
 		bestelbon.setBestelbonLijnen(bestelbonLijnen);
 	}
 
 	@Test
 	public void getTotaalVanBestelBonGivesRightTotal() {
-		assertEquals(0, new BigDecimal(20).compareTo(bestelbon.getTotaalVanBestelBon()));
+		assertEquals(0, new BigDecimal(10).compareTo(bestelbon.getTotaalVanBestelBon()));
 	}
 }
