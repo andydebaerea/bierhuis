7bbad40d4b9bce263d6f1979e842ecb550ff9221
Andy Debaere <andydebaerea@gmail.com>
2014-02-21 13:05:39 +0100

    @PropertySource update

--- src/test/java/be/vdab/dao/DataSourceTest.java
+++ src/test/java/be/vdab/dao/DataSourceTest.java
@@ -12,17 +12,17 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-	//@RunWith(SpringJUnit4ClassRunner.class)
-	//@ContextConfiguration(classes = CreateDAOBeans.class)
-	//@ActiveProfiles("test")
+	@RunWith(SpringJUnit4ClassRunner.class)
+	@ContextConfiguration(classes = CreateDAOBeans.class)
+	@ActiveProfiles("test")
 	public class DataSourceTest {
 		
 	@Autowired
 	private DataSource dataSource;
 	@Test
 	public void getConnection() throws SQLException {
-		/*try (Connection connection = dataSource.getConnection()) {
+		try (Connection connection = dataSource.getConnection()) {
 			
-		}*/
+		}
 	}
 }
