912e5d960d3b951edacd26b1de19f8b297267c29
Andy Debaere <andydebaerea@gmail.com>
2014-02-24 13:19:04 +0100

    heartbeat voor cultuurhuis toegevoegd

--- src/main/java/be/vdab/services/HeartBeatServiceImpl.java
+++ src/main/java/be/vdab/services/HeartBeatServiceImpl.java
@@ -10,5 +10,6 @@ public class HeartBeatServiceImpl implements HeartBeatService {
 	@Scheduled(fixedRate=60000)
 	public void sendHeartBeat() {
 		restTemplate.getForObject("http://bierhuis.andydeb.cloudbees.net", String.class);
+		restTemplate.getForObject("http://cultuurhuis.andydeb.cloudbees.net", String.class);
 	}
 }
